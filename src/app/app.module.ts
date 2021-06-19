d56570df66a46d1761b720455e64b71d2f200618
Michelle Brito <michele.brito17@gmail.com>
2021-06-19 12:01:52 -0300

    Adicionando componente servicos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
